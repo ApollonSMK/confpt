f676f741160b70a7ee3a70a23c34e92bab70a45c
Kauan Pablo <geral@webproject.pt>
2025-07-25 23:29:38 +0000

    Error: In HTML, <html> cannot be a child of <main>. This will cause a hy

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -15,16 +15,9 @@ export default function AdminRootLayout({
 }>) {
 
   return (
-    <html lang="en" className="scroll-smooth">
-       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased bg-background text-foreground">
+    <>
         {children}
         <Toaster />
-      </body>
-    </html>
+    </>
   );
 }
